55be89b5e4fb0a146de913f464fefa5022e5010f
Rahul Sarathy <rahul@sarathy.org>
2019-02-05 20:08:58 -0800

    green sphere tracking to aruco marker

---
+++ ChessboardDetection/SCNVector3+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  SCNVector3+Extensions.swift
+//  ChessboardDetection
+//
+//  Created by Rahul Sarathy on 2/5/19.
+//  Copyright © 2019 Rahul Sarathy. All rights reserved.
+//
+
+import Foundation
